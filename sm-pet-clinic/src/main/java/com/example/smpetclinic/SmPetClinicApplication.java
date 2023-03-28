d2c54eac667caf7e5b5e868cc1f549573f4464d1
Strahinja <Strahinja@LAPTOP-13HLLGCJ>
2023-03-28 12:33:16 +0200

    Initial Project Add

---
+++ sm-pet-clinic/src/main/java/com/example/smpetclinic/SmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.smpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmPetClinicApplication.class, args);
+	}
+
+}
---
+++ sm-pet-clinic/src/test/java/com/example/smpetclinic/SmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
