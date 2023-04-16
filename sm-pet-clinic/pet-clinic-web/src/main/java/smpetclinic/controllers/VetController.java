3fcd2841675a1054f92a09d921ed43d1e73b6817
Strahinja <Strahinja@LAPTOP-13HLLGCJ>
2023-04-16 20:12:39 +0200

    adding Vets Index Page and controller. closes #12

---
+++ sm-pet-clinic/pet-clinic-web/src/main/java/smpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package smpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
+    public String listsVets() {
+        return "vets/index";
+    }
+}
