4d69215aa5cacbea0ed9c11172c659d709ecd674
Strahinja <Strahinja@LAPTOP-13HLLGCJ>
2023-04-16 16:45:38 +0200

    adding BaseEntity.

--- sm-pet-clinic/pet-clinic-data/src/main/java/smpetclinic/model/Person.java
+++ sm-pet-clinic/pet-clinic-data/src/main/java/smpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package smpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- sm-pet-clinic/pet-clinic-data/src/main/java/smpetclinic/model/PetType.java
+++ sm-pet-clinic/pet-clinic-data/src/main/java/smpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package smpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
